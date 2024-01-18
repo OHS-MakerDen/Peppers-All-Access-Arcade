46f183b1558df9b1240613fc4ea44072c0991a3f
bsteffen12 <154364875+bsteffen12@users.noreply.github.com>
2024-01-17 22:28:06 -0800

    Update movement.py

--- trench_game/movement.py
+++ trench_game/movement.py
@@ -1,6 +1,16 @@
 class Wing:
+    def __init__(self, x_posn, y_posn):
+        self.x = x_posn
+        self.y = y_posn
+        self.direction = 1
+
+    def turn(self):
+        self.direction += 1
+
     def move(self):
-      if 
+      if self.direction == 0
+
+
 class Fighter:
 
 class Bullet:
