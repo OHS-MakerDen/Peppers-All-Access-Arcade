e1d8e6b23b1a744fbe425e3c91f8d8bc03cb3ceb
bsteffen12 <154364875+bsteffen12@users.noreply.github.com>
2024-01-19 10:48:34 -0800

    Update Movement.py

--- trench_game/Movement.py
+++ trench_game/Movement.py
@@ -2,12 +2,21 @@ class Wing:
     def __init__(self, x_posn, y_posn):
         self.x = x_posn
         self.y = y_posn
-        self.direction = 1
 
     def move(self):
-        
+        if p1_button: True:
+            self.y += 1
+        elif p1_button: False:
+            self.y -= 1
 
 
 class Fighter:
+    def __init__(self, x_posn, y_posn):
+        self.x = x_posn
+        self.y = y_posn
+
 
 class Bullet:
+    def __init__(self, x_posn, y_posn):
+        self.x = x_posn
+        self.y = y_posn
