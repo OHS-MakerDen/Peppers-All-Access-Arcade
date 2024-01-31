3774b231d89c414df9258e2a813bbb81cb3e37ef
bsteffen12 <154364875+bsteffen12@users.noreply.github.com>
2024-01-31 10:30:28 -0800

    Add files via upload

---
+++ trench_game1/movement.py
@@ -0,0 +1,26 @@
+class Wing:
+    def __init__(self, x_posn, y_posn):
+        self.x = x_posn
+        self.y = y_posn
+
+class Fighter:
+    def __init__(self, x_posn, y_posn):
+        self.x = x_posn
+        self.y = y_posn
+
+
+class Bullet:
+    def __init__(self, x_posn, y_posn):
+        self.x = x_posn
+        self.y = y_posn
+
+    def move(random):
+        if shot:
+            self.x += 1
+            sleep(0.15)
+            self.x += 1
+            sleep(0.15)
+            self.x += 1
+            sleep(0.15)
+            self.x += 1
+
