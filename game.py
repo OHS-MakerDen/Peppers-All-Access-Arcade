f8f77525ec97daa86ad8812b1421a676def18e8d
BobbyOtt07 <154365192+BobbyOtt07@users.noreply.github.com>
2024-02-05 10:50:32 -0800

    Add files via upload

--- game.py
+++ game.py
@@ -2,12 +2,14 @@ import displayio
 import terminalio
 from adafruit_display_text import label
 import time
+from axe.AxeClass import *
 
 game_group = displayio.Group()
 
 axe = displayio.OnDiskBitmap("axe/axe.bmp")
 startingbg = displayio.OnDiskBitmap("axe/STARTINGBG.bmp")
 meter = displayio.OnDiskBitmap("axe/Meter.bmp")
+ndl = displayio.OnDiskBitmap("axe/Needle.bmp")
 first_map = displayio.OnDiskBitmap("axe/map_1.bmp")
 game_over= displayio.OnDiskBitmap("axe/game_over.bmp")
 
@@ -15,6 +17,7 @@ map_1 = displayio.TileGrid(first_map, pixel_shader=first_map.pixel_shader)
 mtr = displayio.TileGrid(meter, pixel_shader=meter.pixel_shader)
 bkgnd = displayio.TileGrid(startingbg, pixel_shader=startingbg.pixel_shader)
 gameover = displayio.TileGrid(game_over, pixel_shader=game_over.pixel_shader)
+needle = displayio.TileGrid(ndl, pixel_shader=ndl.pixel_shader)
 
 FIELD_W = 64
 FIELD_H = 64
@@ -27,6 +30,7 @@ forest_bg = displayio.TileGrid(
     tile_width=64,
     tile_height=64,
 )
+needle.pixel_shader.make_transparent(29)
 forest_bg.pixel_shader.make_transparent(29)
 forest_bg.y = 64
 mtr.pixel_shader.make_transparent(29)
@@ -46,6 +50,7 @@ def game_frame(p1_button: bool, p2_button: bool, coin_button: bool) -> bool:
     if p1_button and not running:
         game_group.append(map_1)
         game_group.append(mtr)
+        game_group.append(needle)
         running = True
     elif p1_button and running:
         game_end = True
