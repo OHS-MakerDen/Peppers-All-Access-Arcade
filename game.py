64fff03004cf91822b4949cf3bdc5076031f6a37
NoahShea <154369700+NoahShea@users.noreply.github.com>
2024-02-09 12:12:50 -0800

    Update game.py

--- game.py
+++ game.py
@@ -7,23 +7,39 @@ game_group = displayio.Group()
 
 score = 0
 
-note = displayio.OnDiskBitmap("graphics/notetile.bmp")
+notetile = displayio.OnDiskBitmap("graphics/notetile.bmp")
 background = displayio.OnDiskBitmap("graphics/background.bmp")
 bkgnd = displayio.TileGrid(map, pixel_shader=background.pixel_shader)
 
+SPACE_W = 8
+SPACE_H = 7
+
+SPACE_FIELD = displayio.TileGrid(
+    note,
+    pixel_shader=note.pixel_shader,
+    width=SPACE_W,
+    height=SPACE_H,
+    tile_width=8,
+    tile_height=8,
+)
+
+SPACE_FIELD.pixel_shader.make_transparent(29)
+SPACE_FIELD.y = 8
 
 def game_setup(p1_button,p2_button, coin_button):
     """this is called once to initialize your game features"""
-    pass
+    game_group.append(bkgnd)
 
 def game_frame(p1_button: bool, p2_button: bool, coin_button: bool):
     if p1_button:
         score += 1
         print(score)
     
+    
     return False
 
 
 def game_over(p1_button,p2_button, coin_button):
-    gamegroup.remove(bkgnd)
-    print(score)
+    """this should display your game over screen with score also clean up the game_group"""
+    pass
+
