7801022f2a3b779e9455bbdce3e54132dc46fad7
coolrian10 <154364825+coolrian10@users.noreply.github.com>
2024-02-12 22:30:03 -0800

    Update game.py

--- game.py
+++ game.py
@@ -69,7 +69,7 @@ class police:
 
             self.whentoappear = 10000000000000000000000000000000000
     def reset(self):
-        self.whentoappear = self.counter + 10
+        self.whentoappear = self.counter + 5
         self.letsdothis = False
         self.timetoreset = False
         self.walking = True
@@ -80,7 +80,6 @@ police = police(4, 9, 4, 4, 4)
 
 
 
-game_group = displayio.Group()
 
 
 background = displayio.OnDiskBitmap("getaway/jailbackground.bmp")
@@ -90,9 +89,10 @@ prisoner = displayio.OnDiskBitmap("getaway/prisoner sprite.bmp")
 startscreen = displayio.OnDiskBitmap("getaway/Getawaytitle.bmp")
 Box = displayio.OnDiskBitmap("getaway/Box.bmp")
 background2 = displayio.OnDiskBitmap("getaway/TheHOLEOrdeal.bmp")
+winbackground = displayio.OnDiskBitmap("getaway/win.bmp")
 realbackground2 = displayio.TileGrid(background2, pixel_shader = background2.pixel_shader, width = 1, height = 1, tile_width = 64, tile_height = 64)
-
-
+loosebackground = displayio.OnDiskBitmap("getaway/gameover.bmp")
+lose = displayio.TileGrid(loosebackground, pixel_shader = loosebackground.pixel_shader)
 prisoner.pixel_shader.make_transparent(4)
 realbackground2.pixel_shader.make_transparent(29)
 
@@ -106,32 +106,19 @@ actualprisoner = displayio.TileGrid(
 )
 realbackground = displayio.TileGrid(background, pixel_shader=background.pixel_shader)
 policerooms = displayio.TileGrid(background1, pixel_shader = background1.pixel_shader)
-actualguard = displayio.TileGrid(
-    guard,
-    pixel_shader=guard.pixel_shader,
-    width=1,
-    height=1,
-    tile_width=8,
-    tile_height=18,
-)
+win_screen = displayio.TileGrid(winbackground, pixel_shader = winbackground.pixel_shader)
 actualBox = displayio.TileGrid(Box,pixel_shader = Box.pixel_shader, width = 1, height = 1,tile_width = 20, tile_height = 20)
 actualBox.x = 25
 actualBox.y = 42
 
 realstartscreen = displayio.TileGrid(startscreen, pixel_shader=startscreen.pixel_shader)
 
-
+bruh = False
 FIELD_W = 1
 FIELD_H = 1
 
 game_group = displayio.Group()
-game_group.append(realbackground)
 
-game_group.append(actualprisoner)
-game_group.append(actualBox)
-
-
-game_group.append(police.actualguard)
 actualprisoner.y = 44
 staying_still = False
 hiding = False
@@ -141,12 +128,13 @@ box_coords = coords(actualBox.x,actualBox.y,20,20)
 amount_of_boxes = [box_coords]
 rotatebackground = True
 endgame = 0
+start = False
 def game_setup(p1_button, p2_button, coin_button):
     """this is called once to initialize your game features"""
-    # while coin_button=false
-    # game_group.append(realstartscreen)
+    game_group.insert(0,realstartscreen)
 
-    yes = True
+
+
 
 
 def game_frame(p1_button, p2_button, coin_button) -> bool:
@@ -155,86 +143,119 @@ def game_frame(p1_button, p2_button, coin_button) -> bool:
     global staying_still
     global rotatebackground
     global endgame
-    police.counters()
-    prisoner_coords = coords(actualprisoner.x,actualprisoner.y,8,12)
-    guard_coords = coords(police.actualguard.x,police.actualguard.y,8,18)
-    box_coords = coords(actualBox.x,actualBox.y,20,20)
-    hiding = False
-    if police.letsdothis == True:
+    global bruh
+    global start
 
-    
-        if police.timetogetcaught == True:
-            police.appear()
-
-        if police.timetomove == True:
-            police.move()
-        if police.timetoreset == True:
-            police.reset()
-  
-  
-    for i in amount_of_boxes:
-        if hiding == True:
-            continue
-        if prisoner_coords.x >= i.x and prisoner_coords.x <= i.x1 and prisoner_coords.x1 >= i.x and prisoner_coords.x1<= i.x1:
-            hiding = True
-    
-    
-    print(hiding)
-            
-    if prisoner_coords.x >= guard_coords.x and prisoner_coords.x <= guard_coords.x1 and hiding == False:
-        print("caught")
-        return True
-    elif prisoner_coords.x1 >= guard_coords.x and prisoner_coords.x1 <= guard_coords.x1 and hiding == False:
-        print("caught")
-        return True
-    
-
-
-
-
-
-
-    if p1_button:
-        actualprisoner.x -= 0
-        actualprisoner[0] = 0
-        staying_still = True
-        
-    if actualprisoner.x >= 64 and endgame == 4:
-        game_group.pop(0)
-        game_group.insert(0,realbackground2)
-        actualBox.x = 20
-        endgame = 10
-        rotatebackground = -1000013
-    elif actualprisoner.x >=64 and rotatebackground == True:
-        actualprisoner.x = 3
-        game_group.remove(realbackground)
-        game_group.insert(0,policerooms)
-        rotatebackground = False
-    elif actualprisoner.x >= 64 and rotatebackground == False:
-        actualprisoner.x = 3
-        game_group.remove(policerooms)
+    if p2_button == False and start == False:
+        pass
+    elif p2_button == True and start == False:
+        game_group.remove(realstartscreen)
         game_group.insert(0,realbackground)
-        rotatebackground = True
-        endgame += 1
-    elif endgame == 10 and prisoner_coords.x >= 60:
-        return True
-        print("YOU DID IT")
-        
-    
-        
-    if not p1_button:
-        actualprisoner.x += 30
-        if staying_still == True:
+        game_group.append(actualprisoner)
+        game_group.append(actualBox)
+
+
+
+        game_group.append(police.actualguard)
+        start = True
+    elif start == True:
+        police.counters()
+        prisoner_coords = coords(actualprisoner.x,actualprisoner.y,8,12)
+        guard_coords = coords(police.actualguard.x,police.actualguard.y,8,18)
+        box_coords = coords(actualBox.x,actualBox.y,20,20)
+        hiding = False
+        if police.letsdothis == True:
+
+
+            if police.timetogetcaught == True:
+                police.appear()
+
+            if police.timetomove == True:
+                police.move()
+            if police.timetoreset == True:
+                police.reset()
+
+
+        for i in amount_of_boxes:
+            if hiding == True:
+                continue
+            if prisoner_coords.x >= i.x and prisoner_coords.x <= i.x1 and prisoner_coords.x1 >= i.x and prisoner_coords.x1<= i.x1:
+                hiding = True
+
+
+        print(hiding)
+
+        if prisoner_coords.x >= guard_coords.x and prisoner_coords.x <= guard_coords.x1 and hiding == False:
+            print("caught")
+            bruh = True
+            return True
+        elif prisoner_coords.x1 >= guard_coords.x and prisoner_coords.x1 <= guard_coords.x1 and hiding == False:
+            print("caught")
+            bruh = True
+            return True
+
+
+
+
+
+
+
+        if p1_button:
+            actualprisoner.x -= 0
             actualprisoner[0] = 0
-            staying_still = False
-            
-            
-        if staying_still == False:
-            actualprisoner[0] = 1
             staying_still = True
-    return False
+
+        if actualprisoner.x >= 64 and endgame == 4:
+            game_group.pop(0)
+            game_group.insert(0,realbackground2)
+            actualBox.x = 20
+            endgame = 10
+            police.actualguard.x = 60
+            rotatebackground = -1000013
+        elif actualprisoner.x >=64 and rotatebackground == True:
+            actualprisoner.x = 3
+            game_group.remove(realbackground)
+            game_group.insert(0,policerooms)
+            rotatebackground = False
+            police.actualguard.x = -10
+            actualBox.x = random.randint(10,40)
+        elif actualprisoner.x >= 64 and rotatebackground == False:
+            actualprisoner.x = 3
+            game_group.remove(policerooms)
+            game_group.insert(0,realbackground)
+            rotatebackground = True
+            police.actualguard.x = -10
+            actualBox.x = random.randint(0,64)
+            endgame += 1
+        elif endgame == 10 and prisoner_coords.x >= 60:
+            police.actualguard.x = 64
+            return True
+            print("YOU DID IT")
+
+
+
+        if not p1_button:
+            actualprisoner.x += 2
+            if staying_still == True:
+                actualprisoner[0] = 0
+                staying_still = False
+            elif staying_still == False:
+                actualprisoner[0] = 1
+                staying_still = True
+        return False
 
 def game_over(p1_button, p2_button, coin_button) -> bool:
     """this should display your game over screen with score also clean up the game_group"""
-    game_group.insert(0,
+    if bruh == True:
+        for index, item in enumerate(game_group):
+            game_group.pop(index)
+        game_group.append(lose)
+    else:
+        for index, item in enumerate(game_group):
+            game_group.pop(index)
+        game_group.append(win_screen)
+
+    while 1:
+        yes = False
+
     pass
